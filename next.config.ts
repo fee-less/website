53eb84dbf91da629cdd9553af098f42535ed2b9c
~Icy <adamoscom98@gmail.com>
2025-07-12 18:21:00 +0200

    Linters disabled for build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://randomuser.me/**')],
+    remotePatterns: [new URL("https://randomuser.me/**")],
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   },
 };
 
